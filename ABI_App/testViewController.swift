cdc426268ca39f740958e2c22c2893e58948681d
juan <juanalexislozano011@gmail.com>
2019-11-12 11:58:25 +0800

    test from alex

--- ABI_App/testViewController.swift
+++ ABI_App/testViewController.swift
@@ -14,6 +14,8 @@ class testViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        print("TEST FROM ALEX")
     }
     
 
