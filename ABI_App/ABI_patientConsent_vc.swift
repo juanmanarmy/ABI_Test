15bb55937624d6ad0817771300a55c03d0d545a8
juanalexis <juanalexisaldana011@gmail.com>
2019-11-12 10:38:15 +0800

    TEST

---
+++ ABI_App/ABI_patientConsent_vc.swift
@@ -0,0 +1,41 @@
+//
+//  ABI_patientConsent_vc.swift
+//  ABI_App
+//
+//  Created by Macky on 12/11/2019.
+//  Copyright © 2019 juan. All rights reserved.
+//
+
+import UIKit
+
+class ABI_patientConsent_vc: UIViewController {
+
+    
+    
+    @IBOutlet var pc_nameOfPatient_tf: UITextField!
+    @IBOutlet var pc_date_tf: UITextField!
+    @IBOutlet var pc_signature_view: UIView!
+    
+    @IBAction func pc_enter_button(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
